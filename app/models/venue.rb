09e62c8ca1af27e800631d3330552bb06e852184
Thomas Morris <thomas.imorris@gmail.com>
2014-11-04 13:02:06 +0200

    Add constants

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,10 @@
 class Venue < ActiveRecord::Base
+  #venue_type constants
+  RESTAURANT = 1
+  COFFEE_SHOP = 2
+  PASTRY_SHOP = 3
+
+  #smoking_policy constants
+  All_NON_SMOKING = 1
+  NON_SMOKING_AREA = 2
 end

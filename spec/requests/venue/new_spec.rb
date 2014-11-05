b2402b4a9102f00ac9efb71e27417dcec1906032
Thomas Morris <thomas.imorris@gmail.com>
2014-11-05 08:14:45 +0200

    Make some tests for venues#new

---
+++ spec/requests/venue/new_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe "venues#new" do
+  let!(:venue) { FactoryGirl.create(:venue) }
+  let!(:ui) { VenueNewPage.new(venue) }
+  
+  context "when not logged in" do
+    it "should give an error"
+  end
+
+  context "when logged in as an admin" do
+    before { ui.visit_page }
+    # before { ui.visit_page_as(admin) }
+
+
+    it "should have the right title" do
+      expect(page.title.downcase).to match /edit/
+    end
+
+
+    it "should submit an email when the submit button is pressed" do
+      attrs = FactoryGirl.attributes_for(:venue)
+      puts "&&&& attrs = #{attrs} &&&&"
+
+      expect{ ui.create_new_venue_with_attributes( attrs ) }
+        .to change{ ActionMailer::Base.deliveries.count }.by(1)
+    end
+  end
+end

fdfbe4f09ec7bb779a936f431cba7cd67209a08f
Thomas Morris <thomas.imorris@gmail.com>
2014-11-07 16:22:25 +0200

    Make tests pass by not making lat/lng inputs be readonly

--- spec/requests/venue/new_spec.rb
+++ spec/requests/venue/new_spec.rb
@@ -18,4 +18,24 @@ describe "venues#new" do
       ui.create_new_venue_with_attributes attrs, sender: "asdf@asdf.com"
     }.to change{ ActionMailer::Base.deliveries.count }.by(1)
   end
+
+
+  describe "the email sent" do
+    let!(:sender) { "asdf@asdf.com" }
+    let!(:attrs) { FactoryGirl.attributes_for(:venue) }
+
+    before do
+      ui.create_new_venue_with_attributes attrs, sender: sender
+    end
+
+    it "should have the proper user email address" do
+      expect(ActionMailer::Base.deliveries.last.body.encoded)
+        .to include sender
+    end
+
+    it "should have the proper smoking policy" do
+      expect(ActionMailer::Base.deliveries.last.body.encoded)
+        .to include attrs[:smoking_policy].to_s
+    end
+  end
 end

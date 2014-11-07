634b763482c852c150bd3ba20125f544c6d16600
Silviu <silviu.straliciuc@gmail.com>
2014-11-07 11:16:54 +0200

    refactored some duplicate strings and added readable_smoking_policy

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -10,31 +10,46 @@ class Venue < ActiveRecord::Base
   ALL_NON_SMOKING = 1
   NON_SMOKING_AREA = 2
 
-  def readable_venue_type
-    hash = {
-      RESTAURANT => "Restaurant",
-      COFFEE_SHOP => "Coffee Shop",
-      PASTRY_SHOP => "Pastry Shop"
-    }
+  @@venue_type_hash = {
+    BLANK => "--",
+    RESTAURANT => "Restaurant",
+    COFFEE_SHOP => "Coffee Shop",
+    PASTRY_SHOP => "Pastry Shop"
+  }
 
-    return hash[self.venue_type]
+  @@smoking_policy_hash = {
+    BLANK => "--",
+    ALL_NON_SMOKING => "Entire venue is non-smoking",
+    NON_SMOKING_AREA => "Venue has a non-smoking section",
+    OTHER => "Other",
+  }
+
+
+  def readable_venue_type
+    result = @@venue_type_hash[self.venue_type ? self.venue_type : BLANK]
+    return result
+  end
+
+  def readable_smoking_policy
+    return @@smoking_policy_hash[self.smoking_policy ? self.smoking_policy : BLANK]
   end
 
   def all_venue_types
-    return [ 
-      ["--", BLANK],
-      ["Restaurant", RESTAURANT],
-      ["Coffee Shop", COFFEE_SHOP],
-      ["Pastry Shop", PASTRY_SHOP]
+    result = [
+      [@@venue_type_hash[BLANK], BLANK],
+      [@@venue_type_hash[RESTAURANT], RESTAURANT],
+      [@@venue_type_hash[COFFEE_SHOP], COFFEE_SHOP],
+      [@@venue_type_hash[PASTRY_SHOP], PASTRY_SHOP]
     ]
+    return result
   end
 
   def all_smoking_policies
     return [ 
-      ["--", BLANK],
-      ["Entire restaurant is non-smoking", ALL_NON_SMOKING],
-      ["Restaurant has a non-smoking section", NON_SMOKING_AREA],
-      ["Other", OTHER]
+      [@@smoking_policy_hash[BLANK], BLANK],
+      [@@smoking_policy_hash[ALL_NON_SMOKING], ALL_NON_SMOKING],
+      [@@smoking_policy_hash[NON_SMOKING_AREA], NON_SMOKING_AREA],
+      [@@smoking_policy_hash[OTHER], OTHER]
     ]
   end
 end

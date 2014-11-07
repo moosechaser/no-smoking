d5b2408637d2de419cd6016fcbdea57c99d53394
Thomas Morris <thomas.imorris@gmail.com>
2014-11-07 15:53:48 +0200

    Remove describe block

--- spec/requests/venue/new_spec.rb
+++ spec/requests/venue/new_spec.rb
@@ -4,19 +4,18 @@ describe "venues#new" do
   let!(:venue) { FactoryGirl.create(:venue) }
   let!(:ui) { VenueNewPage.new(venue) }
   
-  context "when logged in as an admin" do
-    before { ui.visit_page }
+  before { ui.visit_page }
 
-    it "should have the right title" do
-      expect(page.title.downcase).to match /new/
-    end
+  it "should have the right title" do
+    expect(page.title.downcase).to match /new/
+  end
 
 
-    it "should submit an email when the submit button is pressed" do
-      attrs = FactoryGirl.attributes_for(:venue)
+  it "should submit an email when the submit button is pressed" do
+    attrs = FactoryGirl.attributes_for(:venue)
 
-      expect{ ui.create_new_venue_with_attributes( attrs, sender: "asdf@asdf.com" ) }
-        .to change{ ActionMailer::Base.deliveries.count }.by(1)
-    end
+    expect{ 
+      ui.create_new_venue_with_attributes attrs, sender: "asdf@asdf.com"
+    }.to change{ ActionMailer::Base.deliveries.count }.by(1)
   end
 end

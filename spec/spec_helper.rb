53ca3bc4d4c303706c4e5ec8d7c898227a9e7e18
Thomas Morris <thomas.imorris@gmail.com>
2014-11-07 14:41:54 +0200

    Make MailerMacros module

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,6 +45,7 @@ Spork.prefork do
     # examples within a transaction, remove the following line or assign false
     # instead of true.
     config.use_transactional_fixtures = true
+    config.include(MailerMacros)
  
     # If true, the base class of anonymous controllers will be inferred
     # automatically. This will be the default behavior in future versions of

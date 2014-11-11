eb0e228e78d26b5ddd765351d50207f506b366cd
Thomas Morris <thomas.imorris@gmail.com>
2014-11-11 10:26:19 +0200

    Add (ugly) search box

--- app/assets/javascripts/map-search.js
+++ app/assets/javascripts/map-search.js
@@ -19,6 +19,56 @@ function initializeSearch() {
     map: map
   });
 
+
+  //The search box
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('map_search'));
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var searchBox = new google.maps.places.SearchBox(
+      /** @type {HTMLInputElement} */(input));
+
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    // for (var i = 0, marker; marker = markers[i]; i++) {
+    //   marker.setMap(null);
+    // }
+
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+
+
   google.maps.event.addListener(map, 'click', function(event) {
     // put the marker where the map was clicked
     marker.setPosition(event.latLng);

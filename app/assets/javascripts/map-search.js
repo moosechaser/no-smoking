3ae07f130dac1a263d7b9b9b90207bb630bdda61
Thomas Morris <thomas.imorris@gmail.com>
2014-11-11 16:28:55 +0200

    Remove unneccessary code for creating place icon

--- app/assets/javascripts/map-search.js
+++ app/assets/javascripts/map-search.js
@@ -50,14 +50,6 @@ function initializeSearch() {
     markers = [];
     var bounds = new google.maps.LatLngBounds();
     for (var i = 0, place; place = places[i]; i++) {
-      var image = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
       // Create a marker for each place.
       var marker = new google.maps.Marker({
         map: map,

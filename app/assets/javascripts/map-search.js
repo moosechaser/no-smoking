98b0e9fb6eac106f3fbb17294e703f4488b6303f
Thomas Morris <thomas.imorris@gmail.com>
2014-12-18 13:24:53 +0200

    Add comment

--- app/assets/javascripts/map-search.js
+++ app/assets/javascripts/map-search.js
@@ -1,23 +1,9 @@
-var defaultIcon = "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|AAAAAA"
-var activeIcon = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+(function(){
+  var defaultIcon = "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|AAAAAA"
+  var activeIcon = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
 
-var markers = [];
-var userPlacedMarker;
-
-// prevent enter from submitting the form
-$(document).ready(function() {
-  $(window).keydown(function(event){
-    if(event.keyCode == 13) {
-      event.preventDefault();
-      return false;
-    }
-  });
-});
-
-function initializeSearch() {
-  var mapElement = document.getElementById('map-search');
-
-  if(!mapElement) return;
+  var markers = [];
+  var userPlacedMarker;
 
   var mapOptions = {
     center: { lat: 46.7667, lng: 23.5833},
@@ -29,118 +15,139 @@ function initializeSearch() {
     streetViewControl: true
   };
 
-  var map = new google.maps.Map(mapElement, mapOptions);
 
-  var marker = new google.maps.Marker({
-    map: map
+  // prevent enter from submitting the form
+  $(document).ready(function() {
+    $(window).keydown(function(event){
+      if(event.keyCode == 13) {
+        event.preventDefault();
+        return false;
+      }
+    });
   });
 
+  function initializeSearch() {
+    var mapElement = document.getElementById('map-search');
 
-  //The search box
-  var input = document.getElementById('venue_name');
-  var searchBox = new google.maps.places.SearchBox(input);
+    if(!mapElement) return;
+
+    var map = new google.maps.Map(mapElement, mapOptions);
+
+    var marker = new google.maps.Marker({
+      map: map
+    });
 
 
-  // Listen for the event fired when the user selects an item from the
-  // pick list. Retrieve the matching places for that item.
-  google.maps.event.addListener(searchBox, 'places_changed', function() {
-    var places = searchBox.getPlaces();
+    //The search box
+    var input = document.getElementById('venue_name');
+    var searchBox = new google.maps.places.SearchBox(input);
 
-    // Don't do anything if nothing is found
-    if (places.length == 0) { return; }
 
-    clearAllMarkers();
+    // ==========================================================
+    // PLACES CHANGED EVENT LISTENER
+    //
+    // Listen for the event fired when the user selects an item from the
+    // pick list. Retrieve the matching places for that item.
+    // ==========================================================
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+      var places = searchBox.getPlaces();
 
-    // For each place, get the place name and location.
-    markers = [];
+      // Don't do anything if nothing is found
+      if (places.length == 0) { return; }
 
-    var bounds = new google.maps.LatLngBounds();
-    for (var i = 0, place; place = places[i]; i++) {
-      // Create a marker for each place.
-      var marker = new google.maps.Marker({
-        map: map,
-        icon: defaultIcon,
-        title: place.name,
-        position: place.geometry.location
-      });
+      clearAllMarkers();
 
-      markers.push(marker);
-      bounds.extend(place.geometry.location);
+      // For each place, get the place name and location.
+      markers = [];
 
-      // mark the marker as "selected" if it's clicked
-      google.maps.event.addListener(marker, "click", function(event) {
-        clearUserMarkers();
-        selectMarker( this );
-      });
+      var bounds = new google.maps.LatLngBounds();
+      for (var i = 0, place; place = places[i]; i++) {
+        // Create a marker for each place.
+        var marker = new google.maps.Marker({
+          map: map,
+          icon: defaultIcon,
+          title: place.name,
+          position: place.geometry.location
+        });
+
+        markers.push(marker);
+        bounds.extend(place.geometry.location);
+
+        // mark the marker as "selected" if it's clicked
+        google.maps.event.addListener(marker, "click", function(event) {
+          clearUserMarkers();
+          selectMarker( this );
+        });
+      }
+
+      map.fitBounds(bounds);
+
+      // if there's only one result, assume that it is the one the user
+      // wanted
+      if (places.length == 1) { selectMarker(markers[0]) }
+    });
+
+   // Bias the SearchBox results towards places that are within the bounds
+   // of the current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function() {
+      var bounds = map.getBounds();
+      searchBox.setBounds(bounds);
+    });
+
+    google.maps.event.addListener(map, 'click', function(event) {
+      // put the marker where the map was clicked
+      placeSelectedMarker(event.latLng, map);
+
+      // set the latitude and longitude
+      $('#input-lat').val(event.latLng.lat());
+      $('#input-lng').val(event.latLng.lng());
+    });
+
+  }
+  google.maps.event.addDomListener(window, 'load', initializeSearch);
+
+
+  // Sets the passed place as being the one selected by the user
+  function selectMarker( marker ){
+    unselectGoogleMarkers();
+
+    //set the hidden fields
+    $('#input-lat').val(marker.getPosition().lat());
+    $('#input-lng').val(marker.getPosition().lng());
+    marker.setIcon( activeIcon )
+  }
+
+
+  function unselectGoogleMarkers(){
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setIcon(defaultIcon);
     }
-
-    map.fitBounds(bounds);
-
-    // if there's only one result, assume that it is the one the user
-    // wanted
-    if (places.length == 1) { selectMarker(markers[0]) }
-  });
-
- // Bias the SearchBox results towards places that are within the bounds
- // of the current map's viewport.
-  google.maps.event.addListener(map, 'bounds_changed', function() {
-    var bounds = map.getBounds();
-    searchBox.setBounds(bounds);
-  });
-
-  google.maps.event.addListener(map, 'click', function(event) {
-    // put the marker where the map was clicked
-    placeSelectedMarker(event.latLng, map);
-
-    // set the latitude and longitude
-    $('#input-lat').val(event.latLng.lat());
-    $('#input-lng').val(event.latLng.lng());
-  });
-
-}
-google.maps.event.addDomListener(window, 'load', initializeSearch);
-
-
-// Sets the passed place as being the one selected by the user
-function selectMarker( marker ){
-  unselectGoogleMarkers();
-
-  //set the hidden fields
-  $('#input-lat').val(marker.getPosition().lat());
-  $('#input-lng').val(marker.getPosition().lng());
-  marker.setIcon( activeIcon )
-}
-
-
-function unselectGoogleMarkers(){
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setIcon(defaultIcon);
   }
-}
 
 
-function clearUserMarkers() {
-  if (typeof userPlacedMarker !== "undefined") {
-    userPlacedMarker.setMap(null);
-    userPlacedMarker = undefined;
+  function clearUserMarkers() {
+    if (typeof userPlacedMarker !== "undefined") {
+      userPlacedMarker.setMap(null);
+      userPlacedMarker = undefined;
+    }
   }
-}
 
 
-function clearAllMarkers(){
-  for (var i = 0, marker; marker = markers[i]; i++) {
-    marker.setMap(null);
+  function clearAllMarkers(){
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
   }
-}
 
 
-function placeSelectedMarker(position, map) {
-  clearUserMarkers();
+  function placeSelectedMarker(position, map) {
+    clearUserMarkers();
 
-  userPlacedMarker = new google.maps.Marker({
-    position: position,
-    map: map
-  });
+    userPlacedMarker = new google.maps.Marker({
+      position: position,
+      map: map
+    });
 
-  selectMarker(userPlacedMarker);
-}
+    selectMarker(userPlacedMarker);
+  }
+})();
